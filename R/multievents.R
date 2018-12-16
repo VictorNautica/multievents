679f20870245b3470011b9a0460c7e844b7a4312
VictorNautica <45132576+VictorNautica@users.noreply.github.com>
2018-12-16 17:51:31 +0000

    Initial commit

---
+++ R/multievents.R
@@ -0,0 +1,10 @@
+#' Combined Events Package
+#'
+#' Calculate descriptive statistics and generate visualisations of completed decathlons
+#'
+#' @docType package
+#'
+#' @author Victor Yu \email{victoryu@sent.com}
+#'
+#' @name multievents
+NULL
