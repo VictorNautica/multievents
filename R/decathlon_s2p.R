6d9ea502e80a1441c524c5f5c5e70b22fbd159c8
Victor <victoryu@sent.com>
2019-06-02 20:18:00 +0100

    more natural syntax

--- R/decathlon_s2p.R
+++ R/decathlon_s2p.R
@@ -18,29 +18,61 @@
 #' @export
 
 decathlon_s2p <- function(X100m, LJ, SP, HJ, X400m, X110mh, DT, PV, JT, X1500m) {
-  points100m <- floor(25.4347*((18-X100m)^1.81)) # 100m
-  pointslj <- floor(0.14354*(((LJ*100)-220)^1.4)) # LJ
-  pointssp <- floor(51.39*((SP-1.5)^1.05)) # SP
-  pointshj <- floor(0.8465*(((HJ*100)-75)^1.42)) # HJ
-  points400m <- floor(1.53775*((82-X400m)^1.81)) # 400m
-  points110mh <- floor(5.74352*((28.5-X110mh)^1.92)) # 110mh
-  pointsdt <- floor(12.91*((DT-4)^1.1)) # DT
-  pointspv <- floor(0.2797*(((PV*100)-100)^1.35)) # PV
-  pointsjt <- floor(10.14*((JT-7)^1.08)) # JT
-  points1500m <- floor(0.03768*((480-X1500m)^1.85)) # 1500m
+
+library(dplyr); library(lubridate); library(MESS)
+
+  runs_func <- function(event, a, b, c) {
+    if (event < b) {
+      floor(a * ((b - event) ^ c))
+    } else {
+      return(0)
+    }
+  }
+  jumps_func <- function(event, a, b, c) {
+    if ((event*100) > b) {
+      floor(a * (((event * 100) - b) ^ c))
+    } else {
+      return(0)
+    }
+  }
+  throws_func <- function(event, a, b, c) {
+    if (event > b) {
+      floor(a * ((event - b) ^ c))
+    } else {
+      return(0)
+    }
+  }
+
+
+  points100m <- runs_func(X100m, 25.4347, 18, 1.81)
+  pointslj <- jumps_func(LJ, 0.14354, 220, 1.4)
+  pointssp <- throws_func(SP, 51.39, 1.5, 1.05)
+  pointshj <- jumps_func(HJ, 0.8465, 75, 1.42)
+  points400m <- runs_func(X400m, 1.53775, 82, 1.81)
+  points110mh <- runs_func(X110mh, 5.74352, 28.5, 1.92)
+  pointsdt <- throws_func(DT, 12.91, 4, 1.1)
+  pointspv <- jumps_func(PV, 0.2797, 100, 1.35)
+  pointsjt <- throws_func(JT, 10.14, 7, 1.08)
+  points1500m <- runs_func(X1500m, 0.03768, 480, 1.85)
 
 points_vector <- c(points100m, pointslj, pointssp, pointshj, points400m,
     points110mh, pointsdt, pointspv, pointsjt, points1500m)
 fs <- sum(points_vector)
 
-  return(tibble::tibble(day = c(rep("One", 5), rep("Two", 5), NA),
-                        events = c("100m", "LJ", "SP", "HJ", "400m",
-                                   "110mh", "DT", "PV", "JT", "1500m", "Total"),
-                        score = c(X100m, LJ, SP, HJ, X400m, X110mh, DT, PV, JT, X1500m, NA),
-                        points = c(points_vector, fs),
-                        proportion = c(sapply(points_vector, function(x) round(x/fs, 2)), NA)
+  return(tibble::tibble(day = c(rep("One", 5), rep("Two", 5)),
+                        event = forcats::as_factor(c("100m", "Long Jump", "Shotput", "High Jump", "400m",
+                                   "110m Hurdles", "Discus Throw", "Pole Vault", "Javelin Throw", "1500m")),
+                        score = sapply(c(X100m, LJ, SP, HJ, X400m, X110mh, DT, PV, JT, X1500m),
+                                       function(x){
+                                         if(x %in% c(X100m, X400m, X110mh)) return(paste0(x,"s"))
+                                         if(x %in% c(LJ, SP, HJ, DT, PV, JT)) return(paste0(x,"m"))
+                                         else return(seconds_to_period(X1500m) %>% tolower())
+                                         }
+                                       ),
+                        points = points_vector,
+                        cumulative_points = cumsum(points_vector),
+                        proportion = MESS::round_percent(points_vector)/100,
+                        cumulative_proportion = cumsum((MESS::round_percent(points_vector)/100))
                         )
          )
 }
-
-decathlon_s2p(10.7, 8.02, 16, 2.15, 47.35, 13.6, 46.7, 4.90, 57.7, 280.75)
